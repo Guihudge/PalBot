553d5a9f26740939ad5b37b61bea8a627cbc2df0
Guillaume Dindart <guillaume.dindart@etu.u-bordeaux.fr>
2024-01-22 18:17:42 +0100

    Add ability to get save files

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ load_dotenv()
 token = os.getenv("DISCORD_BOT_TOKEN")
 serverID = int(os.getenv("SERVER_ID"))
 server = discord.Object(id=serverID)
+PalServerPath = os.getenv("PALSERVER_HOME")
 
 bot = commands.Bot(intents=discord.Intents.all(), command_prefix='/')
 
@@ -28,15 +29,23 @@ async def Ping(ctx):
     await ctx.response.send_message("Pong")
 
 @tree.command(name="getserverstatus", description="Get actual server status", guild=server)
-async def GetStatus(ctx):
+async def GetStatus(ctx:discord.Interaction):
     rawStatus = subprocess.run(["systemctl --user status palServer.service"], shell=True, capture_output=True)
     status = rawStatus.stdout.decode()
     await ctx.response.send_message(status)
 
 @tree.command(name="restartserver", description="Ask systemctl to restart server", guild=server)
-async def RestartServer(ctx):
+async def RestartServer(ctx:discord.Interaction):
     subprocess.run(["systemctl --user restart palServer.service"], shell=True, capture_output=True)
     await ctx.response.send_message("Restart in progress...")
 
+@tree.command(name="getsavefile", description="Bot send save file", guild=server)
+async def GetSaveFiles(ctx:discord.Interaction):
+    cmd = "tar cvf saveFile.tar " + PalServerPath + "/Pal/Saved/SaveGames/*"
+    status = subprocess.run([cmd])
+    if status.returncode != 0:
+        await ctx.response.send_message("Save archive cration failed")
+    else:    
+        await ctx.response.send_message("Save file attached", file=r"./saveFile.tar")
 
 client.run(token)
